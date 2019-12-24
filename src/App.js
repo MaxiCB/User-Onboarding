11957844cb334c4fd70b3a29a57006add34439d0
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-24 00:46:11 -0600

    Completed project and Stretch goals.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { Container } from 'reactstrap';
 
 import FormComp from './components/FormComp';
 
 function App() {
+
+  const [userInfo, setUserInfo] = useState([]);
+
+  //This is where I will need to store the users and pass data into a table.
+  const test = data => {
+
+    let users = [...userInfo];
+    users.push(data);
+    setUserInfo(users);
+    // console.log(data);
+  }
+
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo])
+
   return (
     <Container fluid={true} className='mainContainer d-flex justify-content-center'>
-      <FormComp/>
+      <FormComp test={test} userData={userInfo}/>
     </Container>
   );
 }
